0e032e09f2f8ad59aa2bb625aac52b0496fd8c66
santimontironi <santiimontironi@gmail.com>
2026-02-02 14:44:46 -0300

    admin creado en prod

--- backend/routes/admin-routes.js
+++ backend/routes/admin-routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { loginAdmin, logoutAdmin, dashboardAdmin, createAdmin } from "../controllers/admin-controllers.js";
+import { loginAdmin, logoutAdmin, dashboardAdmin } from "../controllers/admin-controllers.js";
 import { verifyToken } from "../middlewares/verify-token.js";
 
 export const router = Router();
 
-router.post("/create-admin", createAdmin);
 router.post("/login", loginAdmin);
 router.post("/logout", logoutAdmin);
 
